cbfcc7444059acbdadb23f6a5b4b2f7aeb09d070
hmartos <hectormartosg@gmail.com>
2016-03-17 23:05:11 +0100

    Added list of hours to select

--- src/js/controller.js
+++ src/js/controller.js
@@ -14,10 +14,20 @@
 
         vm.selectedDate = new Date();
 
+        vm.selectedHour = "";
+
+        //Hardcoded data
+        vm.availableHours = ["10:00", "10.30", "11.30", "12.30", "13.00", "17.00", "17.30", "18.00", "18.30", "19.00"];
+
 
         //METHODS
         vm.onSelectDate = function() {
             vm.selectedTab = 1;
+        }
+
+        vm.selectHour = function(hour) {
+            vm.selectedHour = hour;
+            vm.selectedTab = 2;
         }
     }
 
