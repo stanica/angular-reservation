8a8cc79f2fdfd722627f9106e574cc4fea360a5e
Héctor A. Martos Gómez <hmartos@MacBook-Pro-de-Hector.local>
2017-09-10 03:32:13 +0200

    Improvements in tests

--- tests/integration-test.js
+++ tests/integration-test.js
@@ -21,16 +21,25 @@ describe('angular-reservation controller', function () {
         var scope;
         var controller;
         var service;
-        var factory;
+        var factoryMock;
         var config;
 
-        beforeEach(inject(function ($rootScope, $controller, $filter, $translate, _reservationAPIFactory_, _reservationConfig_, _reservationService_) {
+        beforeEach(inject(function ($rootScope, $controller, $filter, $translate, _reservationAPIFactory_, _reservationConfig_, _reservationService_, $q) {
             scope = $rootScope.$new();
 
-           //As this is an integration test, here we should inject services and not mocks
+            //As this is an integration test, here we should inject real service and not mock
             service = _reservationService_;
-            factory = _reservationAPIFactory_;
 
+            //Mock factory to avoid dealing with API's errors
+            factoryMock = {status: "SUCCESS", message: "a message", availableHours: ["10:00", "11:00"]};
+            factoryMock.getAvailableHours = function () {
+                var deferred = $q.defer();
+                deferred.resolve();
+                return deferred.promise;
+            }
+
+            //TODO How to integrate reservationConfig?
+            //Overrride with tour configuration
             config = {
                 getAvailableHoursAPIUrl: "http://localhost:8080/api/availableHours", //API url endpoint to load list of available hours
                 reserveAPIUrl: "http://localhost:8080/api/reserve", //API url endpoint to do a reserve
@@ -44,35 +53,41 @@ describe('angular-reservation controller', function () {
                 confirmationModalTemplate: "confirmationModal.html"
             }
 
-            //_reservationConfig_.set(config);
+            var configProvider = {
+                $get: function () {
+                    return config;
+                },
+                set: function (values) {
+                    angular.extend(config, values);
+                }
+            }
 
-            controller = $controller('ReservationCtrl', {$scope: scope, $filter: $filter, $translate: $translate, reservationAPIFactory: _reservationAPIFactory_, reservationConfig: config, reservationService: _reservationService_});
+            controller = $controller('ReservationCtrl', {
+                $scope: scope,
+                $filter: $filter,
+                $translate: $translate,
+                reservationAPIFactory: factoryMock,
+                reservationConfig: configProvider,
+                reservationService: _reservationService_
+            });
         }));
 
         describe('Date selection logic', function () {
 
-            beforeEach(inject(function ($rootScope, reservationService, reservationAPIFactory) {
+            beforeEach(inject(function ($rootScope, $q, reservationService, reservationAPIFactory) {
                 spyOn(reservationService, 'onBeforeGetAvailableHours').and.callThrough();
 
-                spyOn(reservationAPIFactory, 'getAvailableHours').and.callThrough();
-
                 /*spyOn(viewIncidentService, 'listIncidentTypeProcedure').and.returnValue({
-                    then: function (callbackSuccess, callbackError) {
-                        callbackSuccess(ListIncidentTypeProcedure_OUT);
-                    }
-                });*/
+                 then: function (callbackSuccess, callbackError) {
+                 callbackSuccess(ListIncidentTypeProcedure_OUT);
+                 }
+                 });*/
 
                 //Call onSelectDate function
                 controller.onSelectDate();
-
-                //TODO Organize this. Uncommented executes getAvailableHours callback
-                /*deferred.resolve(); //TODO Pass parameters
-                $rootScope.$digest();
-                expect(factory.getAvailableHours).toHaveBeenCalled();*/
             }));
 
             it('Selected date a valid Date object', inject(function () {
-
                 expect(controller.selectedDate instanceof Date).toBeTruthy();
             }));
 
@@ -95,33 +110,49 @@ describe('angular-reservation controller', function () {
                 //TODO Implement
             }));
 
-            it('getAvailableHours factory method is called', inject(function (reservationConfig, reservationAPIFactory, $rootScope, $q) {
+            it('getAvailableHours factory method is called and response is mapped correctly', inject(function (reservationConfig, reservationAPIFactory, $rootScope) {
                 //TODO Why do I have to do this??
-                var deferred = $q.defer();
-                deferred.resolve();
-                $rootScope.$digest();
-                console.log("Selected date: " + controller.selectedDate);
-                console.log("availableHoursStatus: " + controller.availableHoursStatus);
-                console.log("reservationConfig", reservationConfig);
-                expect(reservationAPIFactory.getAvailableHours).toHaveBeenCalled();
-                expect(controller.availableHoursStatus).toBeDefined();
-                //TODO Pass parameters
+                $rootScope.$digest(); //MUST HAVE call for promise in onBeforeGetAvailableHours to run
+                console.log("reservationConfig", reservationConfig.getAvailableHoursAPIUrl);
+                expect(controller.availableHoursStatus).toBe("SUCCESS");
+                expect(controller.availableHoursMessage).toBe("a message");
+                expect(controller.availableHours).toEqual(["10:00", "11:00"]);
+            }));
+        });
+
+        describe('Hour selection logic', function () {
+
+            beforeEach(inject(function () {
+                //Call selectHour function
+                controller.selectHour("10:00");
             }));
 
-            it('Request status is defined', inject(function ($rootScope, $q) {
-                //expect(controller.availableHoursStatus).toBeDefined();
-                //TODO Implemment
+            it('Select hour correctly', inject(function () {
+                expect(controller.selectedHour).toBe("10:00");
             }));
 
-            it('spec', inject(function ($rootScope, $q) {
-                //expect(controller.loader).toBeTruthy();
-                //TODO Implemment
+            it('Unlock third tab', inject(function () {
+                expect(controller.thirdTabLocked).toBeFalsy();
             }));
 
-            it('spec', inject(function ($rootScope, $q) {
-                //expect(controller.loader).toBeTruthy();
-                //TODO Implemment
+            it('Select hour correctly', inject(function () {
+                expect(controller.selectedTab).toBe(2);
             }));
+        });
+
+        describe('Reserve logic', function () {
+
+            beforeEach(inject(function (reservationService) {
+                spyOn(reservationService, 'onBeforeReserve').and.callThrough();
+
+                //Call selectHour function
+                controller.reserve();
+            }));
+
+            it('onBeforeReserve service method is called', inject(function (reservationService) {
+                expect(reservationService.onBeforeReserve).toHaveBeenCalled();
+            }));
+
         });
 
     });
